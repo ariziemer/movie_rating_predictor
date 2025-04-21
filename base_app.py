732e3a7792be7bebae2ed9b6dbc70c68017feab3
ariziemer <ziemera1995@gmail.com>
2025-04-21 15:53:45 -0400

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -5,8 +5,8 @@ movie_summary = st.text_area("Movie Summary", height=200)
 if movie_summary:
   "query from an LLM, where the user prompt is the movie summary"
   response = openai.chat.completions.create(
-  model="gpt-4o-mini",
-  ...
+    model="gpt-4o-mini",
+    ...
   )
   predicted_rating = response.choices[0].message.content
   st.success(f"Predicted IMDb Rating: {predicted_rating}") #st.success() displays
